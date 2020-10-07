230e74586c5258fd1d6d72adc32b0a9fc14aa673
Karol Moroz <k.j.moroz@gmail.com>
2020-10-07 16:14:50 +0800

    Set up code syntax highlighting

--- lib/api/markdown.ts
+++ lib/api/markdown.ts
@@ -2,6 +2,19 @@ import remark from "remark";
 import html from "remark-html";
 
 export async function formatMarkdown(md: string) {
-  const result = await remark().use(html).process(md);
+  const result = await remark()
+    .use(html)
+    .use(require("@silvenon/remark-smartypants"))
+    .use(require("remark-prism"), {
+      plugins: [
+        "prismjs/plugins/autolinker/prism-autolinker",
+        "prismjs/plugins/diff-highlight/prism-diff-highlight",
+        "prismjs/plugins/inline-color/prism-inline-color",
+        "prismjs/plugins/line-numbers/prism-line-numbers",
+        "prismjs/plugins/treeview/prism-treeview",
+        "prismjs/plugins/show-invisibles/prism-show-invisibles"
+      ]
+    })
+    .process(md);
   return result.toString();
 }
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,7 @@
 import type { AppProps } from "next/app";
 import "../css/app.sass";
+import "prismjs/themes/prism-tomorrow.css";
+import "prismjs/plugins/line-numbers/prism-line-numbers.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
