e03c2dcf0e66f641561f744d8fb16f6649c0d1ca
Karol Moroz <k.j.moroz@gmail.com>
2020-10-07 14:32:43 +0800

    Add a display name to Footer component

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -1,7 +1,9 @@
-export default () => {
+const Footer = () => {
   return (
     <footer className="footer">
       <span>&copy; 2020 by Karol Moroz.</span>
     </footer>
   );
 };
+
+export default Footer;
