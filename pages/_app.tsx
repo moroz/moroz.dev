abff74230ea6c778c652d0aa2ea33bc81f3a6c46
Karol Moroz <k.j.moroz@gmail.com>
2021-02-22 01:35:49 +0800

    Set up Google Analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,22 @@
+import { useEffect } from "react";
 import type { AppProps } from "next/app";
 import "../css/app.sass";
 import "prismjs/themes/prism-tomorrow.css";
 import "prismjs/plugins/line-numbers/prism-line-numbers.css";
+import * as gtag from "../lib/gtag";
+import { useRouter } from "next/router";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
 
