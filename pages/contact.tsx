9d3b4bb87ff3998e91d34ef1e82f2fa5d989b4a7
Karol Moroz <k.j.moroz@gmail.com>
2022-02-21 09:27:17 +0100

    Update contact email

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -8,11 +8,10 @@ const ContactPage = () => {
         <h1 className="page-title mt-0">Get in touch!</h1>
         <p>
           Just drop me a line at{" "}
-          <Link href="mailto:k.j.moroz@gmail.com">
-            <a>k.j.moroz@gmail.com</a>
+          <Link href="mailto:karol@moroz.dev">
+            <a>karol@moroz.dev</a>
           </Link>{" "}
-          to get a quote or if you have any questions regarding my projects or
-          videos.
+          if you have any questions regarding my work.
         </p>
       </div>
     </Layout>
