447693eb87e3fcbe3847f6086f7da66d546bd593
Karol Moroz <k.j.moroz@gmail.com>
2022-06-13 16:15:46 +0200

    Use CSS modules on Landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,20 @@
+import clsx from "clsx";
 import Link from "next/link";
 import Layout from "../layout/Layout";
+import styles from "./Landing.module.sass";
 
 const IndexPage = () => (
   <Layout title="home" className="layout--white-header">
-    <div className="landing">
-      <div className="landing__hero">
+    <div className={styles.landing}>
+      <div className={styles.hero}>
         <p>Hello world. My name is</p>
         <h1>Karol Moroz.</h1>
         <h2>I build modern Web applications.</h2>
-        <div className="landing__description">
+        <div className={styles.description}>
           <p>I am a software developer based in Warsaw, Poland.</p>
         </div>
         <Link href="/contact">
-          <a className="button landing__cta">Get in touch</a>
+          <a className={clsx(styles.cta, "button")}>Get in touch</a>
         </Link>
       </div>
       <picture>
@@ -32,7 +34,7 @@ const IndexPage = () => (
           srcSet="/km.webp"
         />
         <source media="(min-width: 600px)" type="image/png" srcSet="/km.png" />
-        <img src="/km.png" alt="Karol Moroz" className="landing__human" />
+        <img src="/km.png" alt="Karol Moroz" className={styles.human} />
       </picture>
     </div>
   </Layout>
