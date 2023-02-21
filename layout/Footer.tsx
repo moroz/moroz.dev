c11bf2377df2e21a398f91a62ca73452052fe49d
Karol Moroz <karol@moroz.dev>
2023-02-21 02:18:35 +0100

    Bump year

--- layout/Footer.tsx
+++ layout/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./Footer.module.sass";
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      <p>&copy; 2020-2022 by Karol Moroz.</p>
+      <p>&copy; 2020-2023 by Karol Moroz.</p>
       <small>
         The source code of this website is available on{" "}
         <a
