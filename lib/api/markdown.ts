552226de08fd581311cb0fd955b4df907cfdf880
Karol Moroz <karol@moroz.dev>
2023-11-04 16:51:33 +0800

    Fix markdown imports

--- lib/api/markdown.ts
+++ lib/api/markdown.ts
@@ -1,7 +1,6 @@
 import { serialize } from "next-mdx-remote/serialize";
 import { marked } from "marked";
 import rehypeHighlight from "rehype-highlight";
-import { lowlight } from "lowlight/lib/core.js";
 import sass from "highlight.js/lib/languages/scss";
 import elixir from "highlight.js/lib/languages/elixir";
 import swift from "highlight.js/lib/languages/swift";
@@ -10,18 +9,15 @@ import vim from "highlight.js/lib/languages/vim";
 import gql from "highlight.js/lib/languages/graphql";
 import smartypants from "remark-smartypants";
 
-lowlight.registerLanguage("sass", sass);
-lowlight.registerLanguage("elixir", elixir);
-lowlight.registerLanguage("erb", erb);
-lowlight.registerLanguage("swift", swift);
-lowlight.registerLanguage("vim", vim);
-lowlight.registerLanguage("gql", gql);
-lowlight.registerLanguage("graphql", gql);
-
 export async function mdToReact(md: string) {
   return serialize(md, {
     mdxOptions: {
-      rehypePlugins: [rehypeHighlight as any],
+      rehypePlugins: [
+        [
+          rehypeHighlight as any,
+          { languages: { sass, elixir, swift, erb, vim, gql } }
+        ]
+      ],
       remarkPlugins: [smartypants]
     }
   });
