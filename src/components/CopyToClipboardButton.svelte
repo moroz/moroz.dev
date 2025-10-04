50724777d7af080c0ad092d09dc0b7b71cfe7a2a
Karol Moroz <karol@moroz.dev>
2025-10-04 16:05:41 +0800

    First iteration: Svelte copy to clipboard button

--- src/components/CopyToClipboardButton.svelte
+++ src/components/CopyToClipboardButton.svelte
@@ -1,17 +1,34 @@
 <script lang="ts">
   interface Props {
-      lines: string[]
+    lines: string[];
   }
 
-  const {lines} = $props()
+  const { lines } = $props();
+
+  let success = $state(false);
+
+  function onClick() {
+    navigator.clipboard.writeText(lines.join("\n"));
+    success = true;
+
+    setTimeout(() => {
+      success = false;
+    }, 3000);
+  }
 </script>
 
-<button>Copy to clipboard</button>
+<button on:click={onClick}>
+  {#if success}
+    Copied!
+  {:else}
+    Copy to clipboard
+  {/if}
+</button>
 
 <style>
   @reference "@css/global.css";
 
   button {
-      @apply bg-blue-400 text-white p-4;
+    @apply absolute -top-8 right-0 h-8 w-34 cursor-pointer border border-b-0 border-solid border-slate-400 bg-slate-100/40 px-2 text-sm text-slate-900 transition hover:bg-slate-100;
   }
 </style>
