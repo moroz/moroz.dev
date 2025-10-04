28f5891a685eda0be27c390c394934535bcaf45c
Karol Moroz <karol@moroz.dev>
2025-10-05 01:25:56 +0800

    Also fix it on desktop

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -8,7 +8,7 @@
   class="b-unset fixed inset-0 z-10 flex h-16 items-center justify-between border-b border-slate-700 bg-white dark:bg-slate-900"
 >
   <div
-    class="container flex h-full items-center justify-between max-md:w-full max-md:pl-4 md:mx-auto"
+    class="container flex h-full w-full items-center justify-between max-md:pl-4 md:mx-auto"
   >
     <a href="/">
       <h1 class="text-2xl font-bold">moroz.dev</h1>
